ab78273a489ec1981363405a0ae578237ee37a73
Ashraf <zulashrafvns@gmail.com>
2022-12-16 20:57:10 +0800

    fix layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,12 @@ export default function Home() {
   return (
     <div>
       <SectionHeader />
-      <Header />
-      <div className="h-90 w-full bg-cover bg-center p-24 ">
-        {/* <div className="p-8 flex flex-auto justify-center "> */}
-        <div className=" justify-center ">
-          <img src="./img/cover1.png" alt="cover1" />
-          <div className="grid grid-cols-10 gap-4 mt-2">
-          </div>
+      <div className="flex flex-col justify-center items-center p-10">
+        <Header />
+          <div>
+            <img src="./img/cover1.png" alt="cover1" />
+            <div className="grid grid-cols-10 gap-4 mt-2">
+            </div>
         </div>
       </div>
     </div>
